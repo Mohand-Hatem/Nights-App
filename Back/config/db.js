d0ec963428891ad4fd6f21039e884ebb9e0299fa
Mohand hatem <146125801+Mohand-Hatem@users.noreply.github.com>
2025-11-27 18:00:24 +0200

    Update db.js

--- Back/config/db.js
+++ Back/config/db.js
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 
 const ConnectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB, {
-      dbName: "Book-Ecommerce",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_DB);
     console.log("Mongo Database Conected Successfully");
   } catch (error) {
     console.log("Error Database", error);
